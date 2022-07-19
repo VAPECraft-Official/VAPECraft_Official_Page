d4802afe1aada32a5e52842fc16a1e3acffc5c01
Backser Ecation <78302859+Backser0827@users.noreply.github.com>
2022-07-19 19:12:19 +0800

    pre-0.01

---
+++ api/notification/create.php
@@ -0,0 +1,14 @@
+<?php
+// The token in this page was a03jdfc2ef5ff3aea7cen8hl2asse3886
+
+$token = $_POST['token'];
+$content = $_POST['content'];
+
+if ($token != "a03jdfc2ef5ff3aea7cen8hl2asse3886" or !isset($_POST['content'])) {
+    echo json_encode(array("code" => 0, "information" => "Unknown token or content", "state" => "fail"));
+    return 0;
+} else {
+    file_put_contents("notice.txt", date('Y.m.d').' 公告：'.$content);
+    echo json_encode(array("code" => 1, "information" => "Successfully", "state" => "successful"));
+    return 1;
+}
